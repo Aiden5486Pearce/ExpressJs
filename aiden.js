2daadf6d67439c1c9a26a4ec05b406045a7bee05
Aiden5486Pearce <60283775+Aiden5486Pearce@users.noreply.github.com>
2022-05-14 07:40:05 +0530

    Update aiden.js

--- aiden.js
+++ aiden.js
@@ -1,1 +1,23 @@
+const http = require('http');
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+//const port = 3000;
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
+
+app.use('/add-product', (req,res,next)=>{
+    console.log('Hi from here');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="title"><button type="submit">Add Product</buhtton></form>');
+    
+});
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req,res,next)=>{
+    res.send('<h1>Hello from aiden</h1>');
+});
+app.listen(3000);
